a41b42e9097ac3633668047dc0494d37e3dfddbf
Belle9616 <burula.termechikova@gmail.com>
2022-02-17 17:54:19 +0300

    done hw15

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import React,{ Component } from "react";
+import './App.css';
+
+class Todo extends Component {
+  state = {
+    nameOfList: "",
+    lists: []
+  };
+
+  render() {
+     
+    return (
+      <div className="div">
+        <h1>ToDo List</h1>
+        <input
+        className="input"
+          type="text"
+          value={this.state.nameOfList}
+          onChange={this.handleChange.bind(this)}
+          name="nameofList"
+        />
+        <button className="button" onClick={this.handleAddClick.bind(this)}> Add </button>
+        <ul>
+          {this.state.lists.map((data, key) => (
+            <li key={key}>{data}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+  handleChange(e) {
+    this.setState({
+      nameOfList: e.target.value
+    });
+  }
+
+  handleAddClick() {
+    if (!this.state.nameOfList.length) {
+      return;
+    }
+    this.setState({
+      nameOfList: "",
+      lists: [...this.state.lists, this.state.nameOfList]
+    });
+  }
+}
+
+export default Todo;
+
+
